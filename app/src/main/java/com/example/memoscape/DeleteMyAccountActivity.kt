175e8ba6ab8bc71b815794abd028d30dfc56952d
feliciakiani <feliciakiani103@gmail.com>
2023-04-10 11:35:03 +0700

    deleteMyAccount logic init

--- app/src/main/java/com/example/memoscape/DeleteMyAccountActivity.kt
+++ app/src/main/java/com/example/memoscape/DeleteMyAccountActivity.kt
@@ -1,11 +1,79 @@
 package com.example.memoscape
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-class DeleteMyAccountActivity : AppCompatActivity() {
+class DeleteMyAccountActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var confirmDeleteBtn: Button
+    private lateinit var deleteConfirmEdt: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delete_my_account)
+
+        supportActionBar?.hide()
+
+        confirmDeleteBtn = findViewById(R.id.confirm_delete_acc_btn)
+        confirmDeleteBtn.setOnClickListener(this)
+
+        deleteConfirmEdt = findViewById(R.id.delete_confirm_edt)
+
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.confirm_delete_acc_btn -> {
+
+                val deleteConfirmText = deleteConfirmEdt.text.toString()
+                val delete = resources.getString(R.string.delete)
+
+                if(delete.isEmpty()) {
+                    deleteConfirmEdt.error = resources.getString(R.string.account_deletion_confirm)
+
+                } else {
+
+                    val isTextSame = deleteConfirmText.equals(delete, ignoreCase = false)
+
+                    if (isTextSame) {
+
+                        // !!!!!!!!! connect with db
+//                        val db = MyDatabaseHelper(context)
+//                        val email = getterEmail
+//
+//                        val sqlDeleteAccount = "Delete FROM users WHERE email='$email'"
+//                        val success = db.executeNonQuery(sqlDeleteAccount)
+
+                        // FOR NOW ONLY
+                        val success = true
+
+                        if (success) {
+                            Toast.makeText(this, "Account deleted successfully", Toast.LENGTH_SHORT)
+                                .show()
+
+                            // Back to Login Activity
+//                        val intent = Intent(this@DeleteMyAccountActivity, LoginActivity::class.java)
+//                        startActivity(intent)
+
+                        } else {
+                            Toast.makeText(this, "Text does not match with required word.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+
+
+                    }
+                }
+
+
+
+            }
+        }
+    }
+
+
 }
